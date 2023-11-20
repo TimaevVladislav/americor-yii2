7ff705c0cb73435774c063c74b1087a4c68bbeca
timaevV <timaevvladislav@gmail.com>
2023-11-20 16:34:41 +0600

    refactor:notification widget view for improve readability and import

--- widgets/HistoryList/views/_item_common.php
+++ widgets/HistoryList/views/_item_common.php
@@ -1,42 +1,47 @@
 <?php
-
 use app\models\User;
+use app\widgets\DateTime\DateTime;
 use yii\helpers\Html;
 
-/* @var $user User */
-/* @var $body string */
-/* @var $footer string */
-/* @var $footerDatetime string */
-/* @var $bodyDatetime string */
-/* @var $iconClass string */
+/**
+ * Notification Widget View
+ *
+ * @var $user User|null
+ * @var $body string
+ * @var $footer string|null
+ * @var $footerDatetime string|null
+ * @var $bodyDatetime string|null
+ * @var $iconClass string
+ */
 ?>
-<?php echo Html::tag('i', '', ['class' => "icon icon-circle icon-main white $iconClass"]); ?>
 
-    <div class="bg-success ">
-        <?php echo $body ?>
+<?= Html::tag('i', '', ['class' => "icon icon-circle icon-main white $iconClass"]) ?>
 
-        <?php if (isset($bodyDatetime)): ?>
-            <span>
-       <?= \app\widgets\DateTime\DateTime::widget(['dateTime' => $bodyDatetime]) ?>
-    </span>
-        <?php endif; ?>
-    </div>
+<div class="bg-success">
+    <?= $body ?>
+
+    <?php if (isset($bodyDatetime)): ?>
+        <span>
+            <?= DateTime::widget(['dateTime' => $bodyDatetime]) ?>
+        </span>
+    <?php endif; ?>
+</div>
 
 <?php if (isset($user)): ?>
-    <div class="bg-info"><?= $user->username; ?></div>
+    <div class="bg-info"><?= $user->username ?></div>
 <?php endif; ?>
 
 <?php if (isset($content) && $content): ?>
     <div class="bg-info">
-        <?php echo $content ?>
+        <?= $content ?>
     </div>
 <?php endif; ?>
 
 <?php if (isset($footer) || isset($footerDatetime)): ?>
     <div class="bg-warning">
-        <?php echo isset($footer) ? $footer : '' ?>
+        <?= isset($footer) ? $footer : '' ?>
         <?php if (isset($footerDatetime)): ?>
-            <span><?= \app\widgets\DateTime\DateTime::widget(['dateTime' => $footerDatetime]) ?></span>
+            <span><?= DateTime::widget(['dateTime' => $footerDatetime]) ?></span>
         <?php endif; ?>
     </div>
 <?php endif; ?>
